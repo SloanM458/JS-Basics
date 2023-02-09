37d5c19da157e626f306340b7f6839d35e1e69c9
Sloan Molstad <sloan.molstad@gmail.com>
2023-02-09 15:46:25 -0800

    update

--- Button-Clicker/script.js
+++ Button-Clicker/script.js
@@ -5,10 +5,3 @@ function removeButton(element){
     element.remove();
 }
 
-function signIn(element){
-    if(element.innerText ==="Login"){
-        (element.innerText="Log Out")
-    }
-
-    else(element.innerText="Login")
-}
